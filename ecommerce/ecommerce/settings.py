2ef0265467241318c42a165d29891642ccb2089d
mustafabdi100 <106968402+mustafabdi100@users.noreply.github.com>
2024-09-30 12:12:33 +0300

    Update settings.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wnf3kit6msjp&p4b03r-rzh)f@1_s+e=#*s^5$e&xzikknlupa'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
